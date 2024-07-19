2d6eacd7174edc79fbadc1f7d40be5b22a0ed8d2
Nityagopaljena98 <nityagjena@hotmail.com>
2024-07-20 00:43:01 +0530

    first commit

---
+++ JavaScript/DOM/apps/Attributes/due.js
@@ -0,0 +1,10 @@
+let regis = document.getElementById('reg-form');
+let course = document.getElementById('course');
+let totalfees = document.getElementById('total-fee');
+let concession = document.getElementById('con-fees');
+let payfees = document.getElementById('paid-fees');
+let duefees = document.getElementById('due-fees');
+
+let genderBtn = document.querySelector("input[name='caste']:checked").value;
+
+
